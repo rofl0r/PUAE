18c19271e639a7f407545cc3438c893531ec7fab
rofl0r <retnyg@gmx.net>
2014-01-28 16:11:32 +0100

    a2091.h: used type addrbank requires inclusion of memory_uae.h

--- src/include/a2091.h
+++ src/include/a2091.h
@@ -1,4 +1,8 @@
+#ifndef A2091_H
+#define A2091_H
+
 #include "filesys.h"
+#include "memory_uae.h"
 
 #ifdef A2091
 
@@ -34,5 +38,7 @@ int a3000_add_scsi_unit (int ch, struct uaedev_config_info *ci);
 
 int add_scsi_hd (int ch, struct hd_hardfiledata *hfd, struct uaedev_config_info *ci, int scsi_level);
 int add_scsi_cd (int ch, int unitnum);
+
+#endif
 
 #endif
